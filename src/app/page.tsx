963c9c9eceba3dd5fee0c02faa9af789ec9c749c
gabeFrancisco <gabegit2@gmail.com>
2024-05-05 11:34:20 -0300

    adicionado Leaflet e mapa na pagina principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,26 @@
+"use client";
+
+import { Marker, Popup } from "react-leaflet";
+import { MapContainer } from "react-leaflet/MapContainer";
+import { TileLayer } from "react-leaflet/TileLayer";
+import { useMap } from "react-leaflet/hooks";
+
 export default function Home() {
+  const position = [-30.028094, -51.228314];
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex flex-row min-h-screen items-center justify-between p-3">
+      <MapContainer
+        center={position}
+        zoom={12}
+        scrollWheelZoom={true}
+        style={{ height: "90vh", width: "100%", padding: "" }}
+        className="border-2 shadow rounded h-full"
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </main>
   );
 }
