017e8f188bc58a4b66473cd399783b8b7df48499
gabeFrancisco <gabegit2@gmail.com>
2024-05-09 14:59:00 -0300

    criado componente ResgateModal

--- src/app/solicitacao/page.tsx
+++ src/app/solicitacao/page.tsx
@@ -1,27 +1,85 @@
 "use client";
 
+import Modal from "@/components/Modal";
+import ResgateModal from "@/components/ResgateModal";
 import { setCoordenadas } from "@/store/slices/mapSlice";
 import { getSolicitacaoById } from "@/store/slices/solicitacaoSlice";
 import { useAppDispatch, useAppSelector } from "@/store/store";
 import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const page = () => {
   const solicitacao = useAppSelector((state) => state.solicitacoes.solicitacao);
   const dispatch = useAppDispatch();
   const router = useRouter();
+  const [modal, setModal] = useState(false);
+  const [action, setAction] = useState(false);
   useEffect(() => {
     if (solicitacao.createdAt === undefined) {
       router.back();
     }
   }, []);
 
+  const handleModal = () => (modal ? setModal(false) : setModal(true));
+  const handleAction = () => {};
+
   return (
     <div>
+      {modal && (
+        <ResgateModal title="Confirmar resgate">
+          <p>
+            Insira abaixo o código de resgate ou se você não o tiver, envia uma
+            solicitação de conclusão no formulário abaixo:
+          </p>
+          <div className="flex flex-row mt-3 mb-5 items-center">
+            <label htmlFor="codigo" className="text-sm">
+              Código de resgate:{" "}
+            </label>
+            <input
+              type="text"
+              placeholder="Código"
+              className="mx-1 p-0.5 rounded border"
+              maxLength={6}
+            />
+            <button className="bg-green-500 rounded text-sm px-2 py-1 text-white mx-2 hover:bg-green-600">
+              Validar
+            </button>
+          </div>
+          <hr className="my-2" />
+          <div className="flex flex-col">
+            <label htmlFor="mensagem">Mensagem:</label>
+            <textarea
+              rows={5}
+              name="mensagem"
+              id="mensagem"
+              placeholder="Insira a sua mensagem"
+              className="p-1 my-1 text-sm"
+            ></textarea>
+            <button
+              type="button"
+              onClick={handleModal}
+              className="bg-green-500 rounded hover:bg-green-600 shadow p-1 text-white my-2 w-1/3"
+            >
+              Enviar
+            </button>
+          </div>
+          <hr className="my-2" />
+          <div className="flex flex-row justify-between lg:justify-center p-2">
+            <button
+              type="button"
+              onClick={handleModal}
+              className="bg-red-500 rounded  hover:bg-red-600 shadow py-1 px-3 text-white"
+            >
+              Cancelar
+            </button>
+          </div>
+        </ResgateModal>
+      )}
+
       <div className="flex flex-row justify-between pb-2">
         <h2>{solicitacao.situacao}</h2>
         <button
-          className="bg-green-500 shadow hover:bg-green-600 rounded px-3 py-1 text-sm text-white"
+          className="bg-green-500 shadow hover:bg-green-600 rounded px-7 py-1 text-sm text-white"
           onClick={() => router.back()}
         >
           Voltar
@@ -72,7 +130,10 @@ const page = () => {
         <p>Cidade: {solicitacao.endereco.cidade}</p>
         <p>CEP: {solicitacao.endereco.cep}</p>
       </div>
-      <button className="bg-green-500 rounded py-2 px-3 my-2 w-full text-white font-bold shadow text-lg hover:bg-green-600">
+      <button
+        onClick={handleModal}
+        className="bg-green-500 rounded py-2 px-3 my-2 w-full text-white font-bold shadow text-lg hover:bg-green-600"
+      >
         Fui resgatado!
       </button>
     </div>
---
+++ src/components/ResgateModal.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+interface ModalProps {
+  children: React.ReactNode;
+  title?: string;
+}
+
+const ResgateModal = (props: ModalProps) => {
+  return (
+    <div className="w-screen h-screen fixed top-0 left-0 flex flex-col items-center bg-opacity-70 justify-center bg-gray-400">
+      <div className="bg-gray-100 rounded border w-10/12 lg:w-7/12 p-4 shadow">
+        <div className="flex flex-row justify-between px-3 py-1 border-b text-gray-600 font-semibold">
+          {props.title ?? "Modal"}
+        </div>
+        <div className="text-gray-600 p-2">{props.children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ResgateModal;
